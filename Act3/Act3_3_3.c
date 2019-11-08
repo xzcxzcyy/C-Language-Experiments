38375ac1cd53665e2f28ad4a05b42c0bd37a0064
Banson Xu <xzcxzcyy@126.com>
2019-11-08 19:59:06 +0800

    new: Act3_3_3

---
+++ Act3/Act3_3_3.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+enum Type
+{
+    other, blank
+};
+
+enum State
+{
+    init, bl
+};
+
+int getType(char ch);
+
+int main()
+{
+    setbuf(stdout, NULL);
+    enum State current = init;
+    char ch;
+    while ((ch = getchar()) != EOF)
+    {
+        switch (current)
+        {
+            case init:
+                if (getType(ch) == other)
+                {
+                    putchar(ch);
+                }
+                if (getType(ch) == blank)
+                {
+                    current = bl;
+                }
+                break;
+
+            case bl:
+                if (getType(ch) == blank);
+                if (getType(ch) == other)
+                {
+                    putchar(' ');
+                    putchar(ch);
+                    current = init;
+                }
+                break;
+        }
+    }
+    return 0;
+}
+
+int getType(char ch)
+{
+    switch (ch)
+    {
+        case ' ':
+            return blank;
+
+        default:
+            return other;
+    }
+}
