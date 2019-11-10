ed502d3da8422cedcbeed5cf82fc42f6c7db0517
Banson Xu <xzcxzcyy@126.com>
2019-11-10 10:41:17 +0800

    new: original file, for the purpose of composing a report

--- Act3/Act3_1_original.c
+++ Act3/Act3_1_original.c
@@ -6,21 +6,15 @@ int main()
     printf("本程序判断合数，请输入大于1的整数，Ctrl+z结束\n");
     while (scanf("%d", &x) != EOF)
     {
-        /*
-         * 注意到在循环输入时没有重置 flag, 这就导致只要有一次 flag 被置为1，flag的初值在下次循环就是1
-         */
-        flag = 0;
         for (i = 2, k = x >> 1; i <= k; i++)
         {
-            if (!(x % i))
-                /* !运算符优先级较高 */
+            if (!x % i)
             {
                 flag = 1;
                 break;
             }
         }
-        if (flag == 1)
-            /* 错误地使用了赋值运算符 */
+        if (flag = 1)
         {
             printf("%d 是合数\n", x);
         } else
