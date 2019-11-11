794dcfbd0600f28e053b61ef3df6f87129c40803
Banson Xu <xzcxzcyy@126.com>
2019-11-11 22:40:34 +0800

    optimize: add comments in Act3_3_6 new: graph for Act3_3_6

--- Act3/Act3_3_6.c
+++ Act3/Act3_3_6.c
@@ -18,6 +18,11 @@ double m_abs(double x)
     return x >= 0 ? x : -x;
 }
 
+/*
+ * 将使用递归法搜索符合条件的情况
+ * stage 用来保存已经进行过的场次数
+ * a 和 b分别为甲和乙的当前体力值
+ */
 void search(int stage, double a, double b)
 {
     if (stage == 4)
@@ -30,6 +35,10 @@ void search(int stage, double a, double b)
         return;
     }
 
+    /*
+     * 如果尚未达到阶段4，那么就搜索下一阶段的情况
+     * 有两种可能
+     */
     search(stage + 1, a + b / 4, b - b / 4);
     search(stage + 1, a - a / 4, b + a / 4);
 }
