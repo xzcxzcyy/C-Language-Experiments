20158df42b0e6f0e46e25ba7db87f789f97dba9f
Banson Xu <xzcxzcyy@126.com>
2019-12-25 19:31:23 +0800

    new: act8-3-4

---
+++ Act8/act8-3-4.c
@@ -0,0 +1,52 @@
+/*
+ * 旋转矩阵
+ */
+
+#include <stdio.h>
+
+/*
+ * 根据几何知识，
+ * 坐标 (i, j) 旋转过后为 (line - j, i)
+ */
+
+void spin(int *target, const int *source, int line, int column);
+
+int main()
+{
+    freopen("matrix.in", "r", stdin);
+    freopen("matrix.out", "w", stdout);
+    int n, m;
+    scanf("%d %d", &n, &m);
+    int arr[n][m];
+    int tar[m][n];
+    int i, j;
+    for (i = 0; i < n; ++i)
+    {
+        for (j = 0; j < m; ++j)
+        {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    spin(&tar[0][0], &arr[0][0], n, m);
+    for (i = 0; i < m; ++i)
+    {
+        for (j = 0; j < n; ++j)
+        {
+            printf("%d ", tar[i][j]);
+        }
+        putchar('\n');
+    }
+    return 0;
+}
+
+void spin(int *target, const int *source, int line, int column)
+{
+    int i, j;
+    for (i = 0; i < line; ++i)
+    {
+        for (j = 0; j < column; ++j)
+        {
+            *(target + line * (column - 1 - j) + i) = *(source + column * i + j);
+        }
+    }
+}
